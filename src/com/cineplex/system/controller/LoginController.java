a4142a6f7104a0984a4994168ebd787bf4223b2d
Cesar Lopez <clopez-2026231@kinal.edu.gt>
2026-09-08 23:59:29 -0600

    fix: ajustar guardado de sesion a solo memoria durante la ejecucion

--- src/com/cineplex/system/controller/LoginController.java
+++ src/com/cineplex/system/controller/LoginController.java
@@ -4,13 +4,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import com.cineplex.system.service.AuthService;
 import com.cineplex.system.utils.AlertInformation;
-import com.cineplex.system.utils.SesionPreferencias;
 import com.cineplex.system.utils.Session;
 import com.cineplex.system.utils.ViewFactory;
 
@@ -20,22 +18,14 @@ public class LoginController implements Initializable {
     private TextField txtUsuario;
     @FXML
     private PasswordField pwdPassword;
-    @FXML
-    private CheckBox chkRecordarSesion;
 
     private AlertInformation alertInfo = new AlertInformation();
-    
+
     private final AuthService authService = new AuthService();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //Si ya habia una sesion recordada de una vez anterior, dejamos el
-        //campo de usuario precargado para que sea mas rapido volver a entrar
-        String usuarioRecordado = SesionPreferencias.obtenerUsuarioRecordado();
-        if (usuarioRecordado != null) {
-            txtUsuario.setText(usuarioRecordado);
-            chkRecordarSesion.setSelected(true);
-        }
+        
     }
 
     @FXML
@@ -57,17 +47,7 @@ public class LoginController implements Initializable {
             return;
         }
 
-        //Login correcto: se guarda en la sesion de esta ejecucion...
         Session.setUsuarioActual(usuario);
-
-        //...y, si el Administrador marco la casilla, tambien se recuerda
-        //para que la proxima vez que se abra la app entre directo, sin
-        //volver a pedir usuario y contraseña.
-        if (chkRecordarSesion.isSelected()) {
-            SesionPreferencias.recordarUsuario(usuario);
-        } else {
-            SesionPreferencias.olvidarUsuario();
-        }
 
         ViewFactory viewFactory = new ViewFactory();
         viewFactory.viewHome();
